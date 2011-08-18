803e355c869351470b6746bdf5e2a7c69cd80d6c
arthurrauter <arthurraute@gmail.com>
2011-08-18 17:05:44 -0300

    Little tweaks

--- bbb-bot/src/BbbBot.java
+++ bbb-bot/src/BbbBot.java
@@ -7,14 +7,26 @@ public class BbbBot {
 	String room = new String("");
 	BigBlueButtonClient[] BotArmy;
 	int Army_index = 0;
+	int Array[] = new int[12];
 	
+	public void arrayout(){
+		int i;
+		for (i=0;i<Array.length;i++){
+			System.out.println(Array[i]);
+		}
+	}
 	
 	public static void main (String[] args) {
 		int botCounter = 0;
 		int botsCreated = 0;
 		BbbBot Master = new BbbBot(args);
+		Master.Array[1] = 1;
+		Master.Array[5] = 12;
+		Master.arrayout();
+		
+		
 		for (botCounter=Master.getnBots(); botCounter > 0; botCounter--) {
-			if (Master.makeBot())
+			if (true)//Master.makeBot())
 			{
 				botsCreated++;
 			}
@@ -27,6 +39,7 @@ public class BbbBot {
 		for (i=0;i < args.length; i++) {
 			if (args[i].equals("-n")) {
 				nBots = Integer.parseInt(args[i+1]);
+				BotArmy = new BigBlueButtonClient[nBots];
 			}
 			if (args[i].equals("-r")) {
 				room = args[i+1];
@@ -39,6 +52,21 @@ public class BbbBot {
 			
 	private boolean makeBot(){
 		String name = "HappyBot#" + Integer.toString(Army_index);
+		/*BotArmy[Army_index] = new BigBlueButtonClient();
+		BotArmy[Army_index].getJoinService().load(this.server);
+		BotArmy[Army_index].getJoinService().join(this.room, name , true);
+		if (BotArmy[Army_index].getJoinService().getJoinedMeeting() != null) {
+			BotArmy[Army_index].connectBigBlueButton();
+			System.out.println("return true");
+			Army_index++;
+			return true;
+		}
+		else
+		{
+			System.out.println("return false");
+			return false;
+		}*/
+		
 		BigBlueButtonClient client = new BigBlueButtonClient();
 		client.getJoinService().load(this.server);
 		client.getJoinService().join(this.room, name , true);
@@ -53,6 +81,7 @@ public class BbbBot {
 			System.out.println("return false");
 			return false;
 		}
+		
 	}
 	
 	public int getnBots() {
